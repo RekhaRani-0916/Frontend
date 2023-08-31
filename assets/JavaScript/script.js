340b9d7b659fcfce529d13d24d7f7930e176396f
RekhaRani-0916 <rekharani0916@gmail.com>
2023-08-31 20:35:30 +0530

    Add

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,7 +1,7 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//   }
